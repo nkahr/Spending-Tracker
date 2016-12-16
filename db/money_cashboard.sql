84ca9c36c9ec893429d72d8fdc1e08e012691cb0
Nina Kahr <ntkahr@gmail.com>
2016-12-16 12:00:45 +0000

    create tables in money_cashboard database

---
+++ db/money_cashboard.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE merchants (
+  id SERIAL8 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE users (
+  id SERIAL8 primary key,
+  username VARCHAR(255),
+  funds INT8 
+);
+
+CREATE TABLE tags (
+  id SERIAL8 primary key,
+  label VARCHAR(255)
+  );
+
+CREATE TABLE transactions (
+  id SERIAL8,
+  amount INT8,
+  merchant_id INT8 references merchants(id) ON DELETE CASCADE,
+  user_id INT8 references users(id) ON DELETE CASCADE, 
+  tag_id INT8 references tags(id) ON DELETE CASCADE
+);
